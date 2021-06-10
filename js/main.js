6403193949bb656e2c09577eabae7bf312be175e
Kevin Gager <kevin.gager15@gmail.com>
2021-06-10 13:36:07 -0700

    dixed movie information page

--- js/main.js
+++ js/main.js
@@ -7,9 +7,8 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText){
-  //make request to api using axios
-  // Make a request for a user with a given ID
-  var x =1;
+
+  var x =1; //console.log(searchTest);
   axios.get("https://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US&query=" + searchText)
     .then(function (response) {
       let movies = response.data.results;
@@ -33,7 +32,7 @@ function getMovies(searchText){
 
         <hr>
 
-        <a href="index.html" class="btn btn-primary">Show More</a>
+        <a onclick=loadmore('${searchText}') href="index.html" class="btn btn-primary">Show More</a>
       </div>`;
 
 
@@ -44,6 +43,31 @@ function getMovies(searchText){
       console.log(error);
     });
 }
+
+function loadmore(searchText){
+
+  axios.get("https://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US&query=" + searchText)
+    .then(function (response) {
+      let movies = response.data.results;
+      let output = '';
+      $.each(movies, (index, movie) => {
+        output+=`
+          <div class="col-md-3">
+            <div class="well text-center">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+              <h5>${movie.title}</h5>
+              <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
+            </div>
+          </div>
+        `;
+      });
+      $('#movies').html(output);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
 
 function movieSelected(id){
   sessionStorage.setItem('movieId', id);
@@ -57,7 +81,7 @@ function getMovie(){
   axios.get("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=98325a9d3ed3ec225e41ccc4d360c817")
     .then(function (response) {
     let movie = response.data;
-    //console.log(movie);
+    console.log(movie);
     let output = `
         <div class="row">
           <div class="col-md-4">
@@ -69,7 +93,7 @@ function getMovie(){
               <li class="list-group-item"><strong>Genre:</strong> ${movie.genres[0].name}, ${movie.genres[1].name}</li>
               <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
               <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime} min.</li>
-              <li class="list-group-item"><strong>Direcror:</strong> ${movie.director} min.</li>
+              <li class="list-group-item"><strong>Director:</strong> ${movie.title.Director} min.</li>
 
             </ul>
           </div>
